a5214a53913483b9d200bfd57fa6ef4e616964c9
ls1rius <865212941@qq.com>
2021-04-18 19:51:28 +0800

    去掉注释链接

--- resnet_attention.py
+++ resnet_attention.py
@@ -217,6 +217,3 @@ def resnet152_cbam(pretrained=False, **kwargs):
         now_state_dict.update(pretrained_state_dict)
         model.load_state_dict(now_state_dict)
     return model
-————————————————
-版权声明：本文为CSDN博主「东城西阙」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_35605081/article/details/109705974
